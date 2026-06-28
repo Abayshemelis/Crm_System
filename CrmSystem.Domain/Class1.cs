5c8f0840604e26f32434bd2cf99257c34a1c6964
Amazi <abayshemelisshiferaw@gmail.com>
2026-06-28 18:10:13 +0300

    first commit

---
+++ CrmSystem.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CrmSystem.Domain;
+
+public class Class1
+{
+
+}
---
+++ CrmSystem.Domain/Entities/CrmSystem.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+namespace CrmSystem.Domain.Entities;
+
+public enum UserRole
+{
+    Admin,
+    Manager,
+    SalesRep
+}
+
+public class User
+{
+    public int UserId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public UserRole Role { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
